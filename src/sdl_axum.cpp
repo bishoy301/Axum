347f9f15c239f06ace60d5da0185c67c65336fba
Bishoy Boktor <bishoyboktor@gmail.com>
2019-08-05 19:38:35 -0500

    Some light rendering using the built in SDL renderer

--- src/sdl_axum.cpp
+++ src/sdl_axum.cpp
@@ -15,6 +15,23 @@ bool handle_event(SDL_Event *event)
         case SDL_WINDOWEVENT_RESIZED:
             printf("SDL_WINDOWEVENT_RESIZED (%d, %d)\n", event->window.data1, event->window.data2);
             break;
+        case SDL_WINDOWEVENT_EXPOSED:
+            SDL_Window *window = SDL_GetWindowFromID(event->window.windowID);
+            SDL_Renderer *renderer = SDL_GetRenderer(window);
+            static bool is_white = true;
+            if (is_white)
+            {
+                SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+                is_white = false;
+            }
+            else
+            {
+                SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+                is_white = true;
+            }
+            SDL_RenderClear(renderer);
+            SDL_RenderPresent(renderer);
+            break;
         }
     }
     return should_quit;
@@ -33,17 +50,28 @@ int main(int argc, char *argv[])
                               SDL_WINDOWPOS_UNDEFINED,
                               1920,
                               1080,
-                              SDL_WINDOW_RESIZABLE);
+                              SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
 
-    while (true)
+    //SDL_GLContext gl = SDL_GL_CreateContext(window);
+
+    //glClearColor(0, 0, 0, 1);
+
+    if (window)
     {
-        SDL_Event event;
-        SDL_WaitEvent(&event);
-        if (handle_event(&event))
+        SDL_Renderer *renderer = SDL_CreateRenderer(window,
+                                                    -1,
+                                                    0);
+        while (true)
         {
-            break;
+            SDL_Event event;
+            SDL_WaitEvent(&event);
+            if (handle_event(&event))
+            {
+                break;
+            }
         }
     }
+
     SDL_Quit();
     return 0;
 }
