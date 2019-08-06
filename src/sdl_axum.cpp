c0c40775603c6b289d046e10c63dc20db32ff0e0
Bishoy Boktor <bishoyboktor@gmail.com>
2019-08-05 20:45:38 -0500

    OpenGL set up

--- src/sdl_axum.cpp
+++ src/sdl_axum.cpp
@@ -1,8 +1,58 @@
 #include <SDL2/SDL.h>
 
+#define GL3_PROTOTYPES 1
+#include <GL/glew.h>
+
+bool init();
+bool set_opengl_attributes();
+bool handle_event(SDL_Event *event);
+void cleanup();
+
+bool init()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        printf("Failed to init SDL\n");
+        return false;
+    }
+
+    SDL_Window *window;
+    window = SDL_CreateWindow("Axum",
+                              SDL_WINDOWPOS_UNDEFINED,
+                              SDL_WINDOWPOS_UNDEFINED,
+                              1920,
+                              1080,
+                              SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+
+    SDL_GLContext gl = SDL_GL_CreateContext(window);
+
+    set_opengl_attributes();
+
+    SDL_GL_GetSwapInterval();
+
+#ifndef __APPLE__
+    glewExperimental = GL_TRUE;
+    glewInit();
+#endif
+
+    return true;
+}
+
+bool set_opengl_attributes()
+{
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    return true;
+}
+
 bool handle_event(SDL_Event *event)
 {
     bool should_quit = false;
+    SDL_Window *window = SDL_GetWindowFromID(event->window.windowID);
     switch (event->type)
     {
     case SDL_QUIT:
@@ -15,63 +65,54 @@ bool handle_event(SDL_Event *event)
         case SDL_WINDOWEVENT_RESIZED:
             printf("SDL_WINDOWEVENT_RESIZED (%d, %d)\n", event->window.data1, event->window.data2);
             break;
-        case SDL_WINDOWEVENT_EXPOSED:
-            SDL_Window *window = SDL_GetWindowFromID(event->window.windowID);
-            SDL_Renderer *renderer = SDL_GetRenderer(window);
-            static bool is_white = true;
-            if (is_white)
-            {
-                SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-                is_white = false;
-            }
-            else
-            {
-                SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-                is_white = true;
-            }
-            SDL_RenderClear(renderer);
-            SDL_RenderPresent(renderer);
+        }
+    case SDL_KEYDOWN:
+        switch (event->key.keysym.sym)
+        {
+        case SDLK_ESCAPE:
+            should_quit = true;
+        case SDLK_r:
+            glClearColor(1.0, 0.0, 0.0, 1.0);
+            glClear(GL_COLOR_BUFFER_BIT);
+            SDL_GL_SwapWindow(window);
+            break;
+        case SDLK_g:
+            glClearColor(0.0, 1.0, 0.0, 1.0);
+            glClear(GL_COLOR_BUFFER_BIT);
+            SDL_GL_SwapWindow(window);
+            break;
+        case SDLK_b:
+            glClearColor(0.0, 0.0, 1.0, 1.0);
+            glClear(GL_COLOR_BUFFER_BIT);
+            SDL_GL_SwapWindow(window);
             break;
         }
     }
     return should_quit;
 }
 
+void cleanup()
+{
+    SDL_Quit();
+}
+
 int main(int argc, char *argv[])
 {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    if (!init())
     {
-        //TODO: SDL_Init didn't work
+        return -1;
     }
 
-    SDL_Window *window;
-    window = SDL_CreateWindow("Axum",
-                              SDL_WINDOWPOS_UNDEFINED,
-                              SDL_WINDOWPOS_UNDEFINED,
-                              1920,
-                              1080,
-                              SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
-
-    //SDL_GLContext gl = SDL_GL_CreateContext(window);
-
-    //glClearColor(0, 0, 0, 1);
-
-    if (window)
+    while (true)
     {
-        SDL_Renderer *renderer = SDL_CreateRenderer(window,
-                                                    -1,
-                                                    0);
-        while (true)
+        SDL_Event event;
+        SDL_WaitEvent(&event);
+        if (handle_event(&event))
         {
-            SDL_Event event;
-            SDL_WaitEvent(&event);
-            if (handle_event(&event))
-            {
-                break;
-            }
+            break;
         }
     }
 
-    SDL_Quit();
+    cleanup();
     return 0;
 }
