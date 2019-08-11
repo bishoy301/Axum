8213daeecccd5568c8f77ad76800ac35732afd9b
Bishoy Boktor <bishoyboktor@gmail.com>
2019-08-11 17:12:32 -0500

    Refactoring code and using mmap instead of malloc

--- src/sdl_axum.cpp
+++ src/sdl_axum.cpp
@@ -1,10 +1,15 @@
 #include <SDL2/SDL.h>
-#include <stdlib.h>
+#include <stdio.h>
+#include <sys/mman.h>
 #include <stdint.h>
 
-#define global_variable static
+#ifndef MAP_ANONYMOUS
+#define MAP_ANONYMOUS MAP_ANON
+#endif
+
 #define internal static
 #define local_persist static
+#define global_variable static
 
 typedef int8_t i8;
 typedef int16_t i16;
@@ -16,129 +21,198 @@ typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
 
-global_variable SDL_Texture *texture;
-global_variable void *bitmap_memory;
-global_variable int bitmap_width;
-global_variable int bitmap_height;
-global_variable int bytes_per_pixel;
-
-bool handle_event(SDL_Event *event);
-internal void SDL_resize_texture(SDL_Renderer *renderer, int width, int height);
-internal void SDL_update_window(SDL_Window *window, SDL_Renderer *renderer);
-void cleanup();
-
-bool handle_event(SDL_Event *event)
+struct sdl_offscreen_buffer
 {
-    bool should_quit = false;
-    SDL_Window *window = SDL_GetWindowFromID(event->window.windowID);
-    SDL_Renderer *renderer = SDL_GetRenderer(window);
-    switch (event->type)
-    {
-    case SDL_QUIT:
-        printf("SDL_QUIT\n");
-        should_quit = true;
-        break;
-    case SDL_WINDOWEVENT:
-        switch (event->window.event)
-        {
-        case SDL_WINDOWEVENT_SIZE_CHANGED:
-            SDL_resize_texture(renderer, event->window.data1, event->window.data2);
-            break;
-        case SDL_WINDOWEVENT_FOCUS_GAINED:
-            printf("Focus gained\n");
-            break;
-        case SDL_WINDOWEVENT_EXPOSED:
-            SDL_update_window(window, renderer);
-            break;
-        }
-    case SDL_KEYDOWN:
-        switch (event->key.keysym.sym)
-        {
-        case SDLK_ESCAPE:
-            should_quit = true;
-        case SDLK_r:
-            break;
-        case SDLK_g:
-            break;
-        case SDLK_b:
-            break;
-        }
-    }
-    return should_quit;
+    // NOTE: Pixels are alwasy 32-bits wide, Memory Order BB GG RR XX
+    SDL_Texture *Texture;
+    void *Memory;
+    int Width;
+    int Height;
+    int Pitch;
+};
+
+struct sdl_window_dimension
+{
+    int Width;
+    int Height;
+};
+
+global_variable sdl_offscreen_buffer GlobalBackbuffer;
+
+sdl_window_dimension
+SDLGetWindowDimension(SDL_Window *Window)
+{
+    sdl_window_dimension Result;
+
+    SDL_GetWindowSize(Window, &Result.Width, &Result.Height);
+
+    return (Result);
 }
 
-internal void SDL_resize_texture(SDL_Renderer *renderer, int width, int height)
+internal void
+RenderWeirdGradient(sdl_offscreen_buffer Buffer, int BlueOffset, int GreenOffset)
 {
-    if (bitmap_memory)
+    u8 *Row = (u8 *)Buffer.Memory;
+    for (int Y = 0;
+         Y < Buffer.Height;
+         ++Y)
     {
-        free(bitmap_memory);
-    }
-    if (texture)
-    {
-        SDL_DestroyTexture(texture);
-    }
+        u32 *Pixel = (u32 *)Row;
+        for (int X = 0;
+             X < Buffer.Width;
+             ++X)
+        {
+            u8 Blue = (X + BlueOffset);
+            u8 Green = (Y + GreenOffset);
 
-    texture = SDL_CreateTexture(renderer,
-                                SDL_PIXELFORMAT_ARGB8888,
-                                SDL_TEXTUREACCESS_STREAMING,
-                                width,
-                                height);
-    bitmap_width = width;
-    bitmap_height = height;
-    bitmap_memory = malloc(bitmap_width * bitmap_height * bytes_per_pixel);
+            *Pixel++ = ((Green << 8) | Blue);
+        }
+
+        Row += Buffer.Pitch;
+    }
 }
 
-internal void SDL_update_window(SDL_Window *window, SDL_Renderer *renderer)
+internal void
+SDLResizeTexture(sdl_offscreen_buffer *Buffer, SDL_Renderer *Renderer, int Width, int Height)
 {
-    SDL_UpdateTexture(texture,
+    int BytesPerPixel = 4;
+    if (Buffer->Memory)
+    {
+        munmap(Buffer->Memory,
+               Buffer->Width * Buffer->Height * BytesPerPixel);
+    }
+    if (Buffer->Texture)
+    {
+        SDL_DestroyTexture(Buffer->Texture);
+    }
+    Buffer->Texture = SDL_CreateTexture(Renderer,
+                                        SDL_PIXELFORMAT_ARGB8888,
+                                        SDL_TEXTUREACCESS_STREAMING,
+                                        Width,
+                                        Height);
+    Buffer->Width = Width;
+    Buffer->Height = Height;
+    Buffer->Pitch = Width * BytesPerPixel;
+    Buffer->Memory = mmap(0,
+                          Width * Height * BytesPerPixel,
+                          PROT_READ | PROT_WRITE,
+                          MAP_PRIVATE | MAP_ANONYMOUS,
+                          -1,
+                          0);
+}
+
+internal void
+SDLUpdateWindow(SDL_Window *Window, SDL_Renderer *Renderer, sdl_offscreen_buffer Buffer)
+{
+    SDL_UpdateTexture(Buffer.Texture,
                       0,
-                      bitmap_memory,
-                      bitmap_width * bytes_per_pixel);
-    SDL_RenderCopy(renderer,
-                   texture,
+                      Buffer.Memory,
+                      Buffer.Pitch);
+
+    SDL_RenderCopy(Renderer,
+                   Buffer.Texture,
                    0,
                    0);
-    SDL_RenderPresent(renderer);
+
+    SDL_RenderPresent(Renderer);
 }
 
-void cleanup()
+bool HandleEvent(SDL_Event *Event)
 {
-    SDL_Quit();
+    bool ShouldQuit = false;
+
+    switch (Event->type)
+    {
+    case SDL_QUIT:
+    {
+        printf("SDL_QUIT\n");
+        ShouldQuit = true;
+    }
+    break;
+
+    case SDL_WINDOWEVENT:
+    {
+        switch (Event->window.event)
+        {
+        case SDL_WINDOWEVENT_SIZE_CHANGED:
+        {
+            SDL_Window *Window = SDL_GetWindowFromID(Event->window.windowID);
+            SDL_Renderer *Renderer = SDL_GetRenderer(Window);
+            printf("SDL_WINDOWEVENT_SIZE_CHANGED (%d, %d)\n", Event->window.data1, Event->window.data2);
+        }
+        break;
+
+        case SDL_WINDOWEVENT_FOCUS_GAINED:
+        {
+            printf("SDL_WINDOWEVENT_FOCUS_GAINED\n");
+        }
+        break;
+
+        case SDL_WINDOWEVENT_EXPOSED:
+        {
+            SDL_Window *Window = SDL_GetWindowFromID(Event->window.windowID);
+            SDL_Renderer *Renderer = SDL_GetRenderer(Window);
+            SDLUpdateWindow(Window, Renderer, GlobalBackbuffer);
+        }
+        break;
+        }
+    }
+    break;
+    case SDL_KEYDOWN:
+    {
+        switch (Event->key.keysym.sym)
+        {
+        case SDLK_ESCAPE:
+        {
+            ShouldQuit = true;
+        }
+        break;
+        }
+    }
+    break;
+    }
+
+    return (ShouldQuit);
 }
 
 int main(int argc, char *argv[])
 {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    SDL_Init(SDL_INIT_VIDEO);
+    // Create our window.
+    SDL_Window *Window = SDL_CreateWindow("Axum",
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          640,
+                                          480,
+                                          SDL_WINDOW_MAXIMIZED | SDL_WINDOW_RESIZABLE);
+    if (Window)
     {
-        printf("Failed to init SDL\n");
-        return false;
-    }
-
-    SDL_Window *window;
-    window = SDL_CreateWindow("Axum",
-                              SDL_WINDOWPOS_UNDEFINED,
-                              SDL_WINDOWPOS_UNDEFINED,
-                              1920,
-                              1080,
-                              SDL_WINDOW_MAXIMIZED | SDL_WINDOW_RESIZABLE);
-    if (window)
-    {
-        SDL_Renderer *renderer = SDL_CreateRenderer(window,
+        // Create a "Renderer" for our window.
+        SDL_Renderer *Renderer = SDL_CreateRenderer(Window,
                                                     -1,
                                                     0);
-        if (renderer)
+        if (Renderer)
         {
-            bool running = true;
-            int width, height;
-            SDL_GetWindowSize(window, &width, &height);
-            while (true)
+            bool Running = true;
+            sdl_window_dimension Dimension = SDLGetWindowDimension(Window);
+            SDLResizeTexture(&GlobalBackbuffer, Renderer, Dimension.Width, Dimension.Height);
+            int XOffset = 0;
+            int YOffset = 0;
+            while (Running)
             {
-                SDL_Event event;
-                SDL_WaitEvent(&event);
-                if (handle_event(&event))
+                SDL_Event Event;
+                while (SDL_PollEvent(&Event))
                 {
-                    break;
+                    if (HandleEvent(&Event))
+                    {
+                        Running = false;
+                    }
                 }
+                RenderWeirdGradient(GlobalBackbuffer, XOffset, YOffset);
+                SDLUpdateWindow(Window, Renderer, GlobalBackbuffer);
+
+                ++XOffset;
+                YOffset += 2;
             }
         }
         else
@@ -151,6 +225,6 @@ int main(int argc, char *argv[])
         // TODO: Logging
     }
 
-    cleanup();
-    return 0;
+    SDL_Quit();
+    return (0);
 }
